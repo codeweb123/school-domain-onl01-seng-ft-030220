0ad4b219d57fc244f0fa0741ae3f5da3cb70697f
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-10 18:11:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,30 @@
-def sort
-new_hash = {}
-roster.each do |x, y|
-new_hash[x] = y.sort
+class School
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
 end
-new_hash
-end 
